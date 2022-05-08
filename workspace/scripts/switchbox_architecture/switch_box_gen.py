33abe237f102dd61ae34fcc8515540ee50887c1d
wazala414 <hk19236@bristol.ac.uk>
2022-05-08 02:58:06 +0100

    Initialized personal workspace for experimenting and provides helpful scripts for the use of VTR for routing switchblocks with looping (same side) connections.

---
+++ workspace/scripts/switchbox_architecture/switch_box_gen.py
@@ -0,0 +1,37 @@
+#!/usr/bin/env python3
+
+import sys
+import re
+import os
+import argparse
+
+parser = argparse.ArgumentParser(formatter_class=argparse.ArgumentDefaultsHelpFormatter)
+parser.add_argument("--output_dir", default=".", help="Directory to place arch xml files")
+args = parser.parse_args()
+
+openlist = []
+
+side = ['lt','lr','lb','tr','tb','rb','bb','tt','rr','ll']
+W = 50
+filename = "/home/vm/VTR-Tools/workspace/Basic_Architecture/switchbox_out.xml"
+
+#<func type="lr" formula="t"/>
+
+# def xcopy(fsrc):
+#     src = open(fsrc, "r")
+#     for line in src:
+#         xprint(line)
+#     src.close()
+#     f.write("\n")
+
+f = open(filename, "w")
+perm = None
+for x in range(len(side)):
+    for y in range(int(W/2)):
+        perm = 2*y+1
+        #print(perm)
+        f.write('<func type="')
+        f.write("%s" % side[x])
+        f.write('" formula="t+')
+        f.write("%i" % perm)
+        f.write('"/>\n')
